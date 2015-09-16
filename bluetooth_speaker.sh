b8d5c470a2e8f6ec4cc420a366094fea653afa8a
Pawel Raszewski <raszewski@gmail.com>
2015-09-16 03:53:14 +0000

    hack for wifi

--- bluetooth_speaker.sh
+++ bluetooth_speaker.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-rfkill unblock bluetooth
+/usr/sbin/rfkill unblock bluetooth
+/usr/bin/hciconfig hci0 up
+/usr/bin/hciconfig hci0 piscan
+systemctl start obex
+
 sleep 2
 echo -e "connect 5A:5A:5A:A6:3B:C7\nquit\n" | bluetoothctl
 pactl set-default-sink bluez_sink.5A_5A_5A_A6_3B_C7
-
+# hack for wifi
+ifconfig wlan0 up
