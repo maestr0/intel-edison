3e16b37415ad0dd453b760e0e97bcfc975b66571
Pawel Raszewski <raszewski@gmail.com>
2015-11-22 06:45:03 +0000

    added volume controll on speech

--- bluetooth_speaker.sh
+++ bluetooth_speaker.sh
@@ -10,6 +10,6 @@ echo -e "connect 5A:5A:5A:A6:3B:C7\nquit\n" | bluetoothctl
 sleep 2
 pactl set-default-sink bluez_sink.5A_5A_5A_A6_3B_C7
 # hack for wifi
-sleep 10
-ifconfig wlan0 up
+#sleep 10
+#ifconfig wlan0 up
 
--- speak-cm.sh
+++ speak-cm.sh
@@ -1,8 +1,9 @@
-#!/bin/sh                                                                                                                                                                                                                                                  
+#!/bin/bash                                                                  
+volume=$(($1 - 1))
+
 say() {                                                                                                                                                                                                                                                    
-        echo "$*" | espeak -s120 -k18 -a200 -v en-westindies -w tmp.wav                                                                                                                                                                                    
-#sleep 1                                                                                                                                                                                                                                                   
-gst-launch-1.0 filesrc location=tmp.wav ! wavparse ! volume volume=0.01 ! pulsesink                                                                                                                                                                        
-rm -fr tmp.wav                                                                                                                                                                                                                                             
+	echo "$2" | espeak -s120 -k18 -a200 -v en-westindies -w tmp.wav                                                                                                                                                                                    
+	gst-launch-1.0 filesrc location=tmp.wav ! wavparse ! volume volume=0.${volume} ! pulsesink                                                                                                                                                                        
+	rm -fr tmp.wav                                                                                                                                                                                                                                             
 }                                                                                                                                                                                                                                                          
-say $* 
+say "$@" 
